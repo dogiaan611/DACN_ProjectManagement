591e1488f1d6f2f6ddfb594d54423d47e7efd703
dogiaan611 <dogiaan611@gmail.com>
2025-10-08 00:33:20 +0700

    Test

--- ProjectManagement/obj/Debug/net8.0/ProjectManagement.AssemblyInfo.cs
+++ ProjectManagement/obj/Debug/net8.0/ProjectManagement.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15fb716a3695f9525aa57a584ce9da59a09a492e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cafd9bb55529e01995c91c508efa79efe0e95045")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
